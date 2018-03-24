aa210624d6410dd29ae1a116347b9286c19b1801
Wyatt Roersma <wyattroersma@gmail.com>
2018-03-24 13:08:08 -0600

    YamlInfo first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""YamlInfo Install Script"""
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='yamlinfo',
+    version='0.0.1.1',
+    packages=[''],
+    url='https://github.com/AUCR/YamlInfo',
+    license='GPL-3.0',
+    author='Wyatt Roersma',
+    author_email='wyattroersma@gmail.com',
+    description='Yaml File Config Parser '
+)
---
+++ yamlinfo.py
@@ -0,0 +1,33 @@
+"""YamlInfo Main Module"""
+# coding=utf-8
+import yaml
+import logging
+
+
+class YamlInfo:
+    """Parse Yaml config file."""
+
+    yaml_info_dict = {}
+
+    def __init__(self, yaml_config_file, option, input_file) -> None:
+        """Load yaml information and parse args info."""
+        with open(yaml_config_file, 'rb') as input_config_file_object:
+            yaml_info_strings = input_config_file_object.read()
+        self.yaml_info_dict = yaml.load(yaml_info_strings)
+        if option == "strip":
+            if input_file == "LICENSE":
+                with open("LICENSE") as license_file:
+                    license_strings = license_file.read()
+                self.yaml_info_dict["license"] = license_strings.strip('\n')
+        elif option == "projectinfo":
+            project_info_data = self.yaml_info_dict["info"]
+            project_version_data = self.yaml_info_dict["version"]
+            __version__ = "%(major)s.%(minor)s.%(revision)s" % project_version_data
+            logging.info("Starting yamlinfo v" + __version__)
+            for items in project_info_data:
+                logging.info(str(items) + ":" + str(project_info_data[items]))
+
+    def get(self) -> dict:
+        """Return parsed project config file as a dict."""
+        logging.info("Parsing Yaml Config file!")
+        return self.yaml_info_dict
