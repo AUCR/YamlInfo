f5241159061297bb033db0feccae994ff04d6ae9
Wyatt Roersma <wyattroersma@gmail.com>
2018-03-25 00:04:02 -0600

    Update to v0.0.1.11

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 """YamlInfo Install Script"""
 # coding=utf-8
-from yaml_info.yamlinfo import YamlInfo
 from setuptools import setup, find_packages
+from pkg_resources import require, DistributionNotFound
+try:
+    require('PyYAML')
+    required='PyYAML'
+except DistributionNotFound:
+    required = 'PyYAML'
+
+from yaml_info.yamlinfo import YamlInfo
+
+
 
 project_data = YamlInfo("projectinfo.yml", "projectinfo", "LICENSE").get()
 project_info_data = project_data["info"]
 project_version_data = project_data["version"]
-__version__ = "%(major)s.%(minor)s.%(revision)s" % project_version_data
+__version__ = "%(major)s.%(minor)s.%(revision)s.%(release)s" % project_version_data
 
 setup(
     name=project_data["info"]["name"],
     version=__version__,
-    packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=['PyYAML'],
-    url='https://github.com/AUCR/YamlInfo',
-    license='GPL-3.0',
+    include_package_data=True,
+    install_requires=["PyYAML"],
+    packages=find_packages(exclude=["docs", "tests*"]),
+    url="https://github.com/AUCR/YamlInfo",
+    license=project_data["info"]["license"],
     author=project_data["info"]["authors"],
     author_email=project_data["info"]["authors_email"],
     description=project_data["info"]["description"],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.6',
-    ],
+    classifiers=project_data["info"]["classifiers"],
 )
